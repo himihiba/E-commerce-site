56fb0d564b683cb88fb19c5615a5f7175469f125
h.hiba <hiba.himi@ensttic.dz>
2024-08-10 11:01:59 +0200

    page2H

--- js/main.js
+++ js/main.js
@@ -34,9 +34,35 @@ const shadowHeader = () =>{
 }
 window.addEventListener('scroll',shadowHeader);
 
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(checkbox => {
+            checkbox.addEventListener('change', function() {
+                if (this.checked) {
+                    checkboxes.forEach(box => {
+                        if (box !== this) box.checked = false;
+                    });
+                }
+            });
+});
 
-const scrollUp = ()=>{
-    const scrollUp = document.getElementById('scroll-up');
-    this.scrollY>=350 ? scrollUp.classList.add('show-scroll'):scrollUp.classList.remove('show-scroll')
-}
-window.addEventListener('scroll',scrollUp)
+const stars = document.querySelectorAll('.stars i');
+
+        stars.forEach(star => {
+            star.addEventListener('click', function() {
+                const rating = this.getAttribute('data-value');
+
+                
+                stars.forEach(s => s.classList.remove('active'));
+
+                stars.forEach(s => {
+                    if (s.getAttribute('data-value') <= rating) {
+                        s.classList.add('active');
+                    }
+                });
+            });
+});
+// const scrollUp = ()=>{
+//     const scrollUp = document.getElementById('scroll-up');
+//     this.scrollY>=350 ? scrollUp.classList.add('show-scroll'):scrollUp.classList.remove('show-scroll')
+// }
+// window.addEventListener('scroll',scrollUp)
